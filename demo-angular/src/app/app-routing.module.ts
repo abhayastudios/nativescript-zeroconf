b4fb2ab48969a16eda46313964c69d18ccb96ce6
Jonathan Salomon <mail2joni@gmail.com>
2020-02-08 12:53:43 +0200

    Updated demo-angular to NativeScript 6.3

---
+++ demo-angular/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { Routes } from "@angular/router";
+
+import { ZeroconfComponent } from "./zeroconf/zeroconf.component";
+
+const routes: Routes = [
+    { path: "", redirectTo: "/zeroconf", pathMatch: "full" },
+    { path: "zeroconf", component: ZeroconfComponent },
+];
+
+@NgModule({
+    imports: [NativeScriptRouterModule.forRoot(routes)],
+    exports: [NativeScriptRouterModule]
+})
+export class AppRoutingModule { }
